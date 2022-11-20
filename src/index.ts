24080ee7262f1659c2b2b01d1babb981da16e54c
wr1472 <wadud.ruf@gmail.com>
2022-11-20 17:21:33 +0000

    Cat and Dog extend BasePet but expose Pet.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import "source-map-support/register";
 
 import Express from "express";
 import morgan from "morgan";
-import type {Document, Request} from "openapi-backend";
-import OpenAPIBackend from "openapi-backend";
-import swaggerUi from 'swagger-ui-express';
+import OpenAPIBackend, {Document, Request} from "openapi-backend";
+import * as swaggerUi from 'swagger-ui-express';
 
-import definition from "../api/pets.json";
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+
+import schema from "../api/pets.json";
 
 const pets = [
     {name: "shitzu", "petType": "Cat", huntingSkill: "clueless"},
@@ -15,49 +16,51 @@ const pets = [
     {name: "alsatian", "petType": "Dog", packSize: 1},
 ];
 
-const app = Express();
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(definition));
-app.use(Express.json());
+(async () => {
+    const definition = (await $RefParser.dereference(schema)) as Document;
+    const app = Express();
+    
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(definition));
+    app.use(Express.json());
+    // console.log(JSON.stringify(definition, null, 4))
 
-// define api
-const api = new OpenAPIBackend({
-    apiRoot: "/api",
-    definition: definition as Document,
-    handlers: {
-        getPets: async (c, req: Express.Request, res: Express.Response) => {
-            res.status(200)
-                .json({operationId: c.operation.operationId, pets})
+    const api = new OpenAPIBackend({
+        apiRoot: "/api",
+        definition,
+        ajvOpts: {
+            discriminator: true,
         },
-        getCats: async (c, req: Express.Request, res: Express.Response) => {
-            res.status(200)
-                .json({operationId: c.operation.operationId, cats: pets.filter(pet => pet.petType === "Cat")})
+        handlers: {
+            getPets: async (c, req: Express.Request, res: Express.Response) => {
+                res.status(200)
+                    .json({operationId: c.operation.operationId, pets})
+            },
+            getCats: async (c, req: Express.Request, res: Express.Response) => {
+                res.status(200)
+                    .json({operationId: c.operation.operationId, cats: pets.filter(pet => pet.petType === "Cat")})
+            },
+            
+            getPetById: async (c, req: Express.Request, res: Express.Response) =>
+                res.status(200)
+                    .json({operationId: c.operation.operationId}),
+
+            createPet: async (c, req: Express.Request, res: Express.Response) =>
+                res.status(201)
+                    .json({operationId: c.operation.operationId}),
+            createCat: async (c, req: Express.Request, res: Express.Response) =>
+                res.status(201)
+                    .json({operationId: c.operation.operationId}),
+
+            validationFail: async (c, req: Express.Request, res: Express.Response) =>
+                res.status(400)
+                    .json({err: c.validation.errors}),
+            notFound: async (c, req: Express.Request, res: Express.Response) => res.status(404).json({message: "not found"}),
         },
-        
-        getPetById: async (c, req: Express.Request, res: Express.Response) =>
-            res.status(200)
-                .json({operationId: c.operation.operationId}),
+    });
 
-        createPet: async (c, req: Express.Request, res: Express.Response) =>
-            res.status(201)
-                .json({operationId: c.operation.operationId}),
-        createCat: async (c, req: Express.Request, res: Express.Response) =>
-            res.status(201)
-                .json({operationId: c.operation.operationId}),
+    api.init();
+    app.use(morgan("combined"));
+    app.use((req, res) => api.handleRequest(req as Request, req, res));
 
-        validationFail: async (c, req: Express.Request, res: Express.Response) =>
-            res.status(400)
-                .json({err: c.validation.errors}),
-        notFound: async (c, req: Express.Request, res: Express.Response) => res.status(404).json({message: "not found"}),
-    },
-});
-
-api.init();
-
-// logging
-app.use(morgan("combined"));
-
-// use as express middleware
-app.use((req, res) => api.handleRequest(req as Request, req, res));
-
-// start server
-app.listen(9000, () => console.info("api listening at http://localhost:9000"));
+    app.listen(9000, () => console.info("api listening at http://localhost:9000"));
+})();
